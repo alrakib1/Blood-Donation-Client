21a017b2247f9dfbced45bed56b492bc1b31088b
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-01-06 11:42:15 +0600

    added loading skeleton

--- client/src/pages/All Request/AllRequest.jsx
+++ client/src/pages/All Request/AllRequest.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 import Skeleton from "@mui/material/Skeleton";
-import Stack from "@mui/material/Stack";
+
 import { useState } from "react";
+import LoadingSkeleton from "./LoadingSkeleton";
 
 const AllRequest = () => {
   const axiosPublic = useAxiosPublic();
@@ -29,252 +30,83 @@ const AllRequest = () => {
   };
 
   return (
-    <div className=" max-w-screen-xl mx-auto my-10 border rounded-md px-5">
-      <Helmet>
-        <title>LifeFlowDonor | All</title>
-      </Helmet>
+    <div className="max-w-screen-xl mx-auto my-10 border rounded-md">
+    <Helmet>
+      <title>LifeFlowDonor | All</title>
+    </Helmet>
 
-      <div className="text-white">
-        <div className="flex justify-between items-center gap-5">
-          <h3 className="text-2xl font-semibold text-center py-10">
-            All Requests
-          </h3>
-          <div className="text-base">
-            <select
-              onChange={handleSelect}
-              className=" py-1 px-1 rounded-md border w-full max-w-xs bg-[#8B0000]"
-            >
-              <option value="">All</option>
-              <option value="inprogress">Inprogress</option>
-              <option value="canceled">Canceled</option>
-              <option value="done">Done</option>
-            </select>
-          </div>
-        </div>
-        <div className="overflow-x-auto">
-          <table className="table table-xs mb-7">
-            <thead>
-              <>
-                {isLoading ? (
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                ) : (
-                  <>
-                    <tr className="text-white">
-                      <th>No</th>
-                      <th> Recipient Name</th>
-                      <th>Require Blood Group</th>
-                      <th> Location</th>
-                      <th>Donation Date</th>
-                      <th>Donation Time</th>
-                      <th>Donation Status</th>
-                      <th>Donor Name</th>
-                      <th>Donor Email</th>
-                    </tr>
-                  </>
-                )}
-              </>
-            </thead>
-            <tbody>
-              {isLoading ? (
-                <Stack spacing={2}>
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                  <Skeleton
-                    width="100vw"
-                    height={30}
-                    variant="text"
-                    sx={{ fontSize: "1rem" }}
-                  />
-                </Stack>
-              ) : (
-                <>
-                  {filteredRequests.map((request, index) => (
-                    <tr key={request._id}>
-                      <th>{index + 1}</th>
-                      <td>{request.recipientName}</td>
-                      <td>{request.requiredBloodGroup}</td>
-                      <td>
-                        {request.upazila},{request.district}
-                      </td>
-                      <td>{request.donationDate}</td>
-                      <td>{request.donationTime}</td>
-                      <td>{request.donationStatus}</td>
-                      <td>
-                        {request?.donationStatus === "inprogress" ? (
-                          <>{request.donorName}</>
-                        ) : (
-                          <>{request.donorName || "None"}</>
-                        )}
-                      </td>
-                      <td>
-                        {request?.donationStatus === "inprogress" ? (
-                          <>{request.donorEmail}</>
-                        ) : (
-                          <>{request.donorEmail || "None"}</>
-                        )}
-                      </td>
-
-                      <td className="flex gap-3">
-                        <Link to={`/dashboard/details/${request._id}`}>
-                          <button className="btn btn-xs">view</button>
-                        </Link>
-                      </td>
-                    </tr>
-                  ))}
-                </>
-              )}
-            </tbody>
-          </table>
+    <div className="text-white text-sm">
+      <div className="flex flex-col md:flex-row justify-around items-center gap-5">
+        <h3 className="text-xl font-semibold text-center py-10">
+          All Requests
+        </h3>
+        <div className="mb-5 md:mb-0">
+          <select
+            onChange={handleSelect}
+            className="py-1 px-1 rounded-md border w-full max-w-xs md:w-auto bg-[#8B0000]"
+          >
+            <option value="">All</option>
+            <option value="inprogress">Inprogress</option>
+            <option value="canceled">Canceled</option>
+            <option value="done">Done</option>
+          </select>
         </div>
       </div>
+      <div className="overflow-hidden">
+        <table className="table table-xs mb-7 w-full">
+          <thead>
+            <>
+              {isLoading ? (
+                <Skeleton
+                  width="100%"
+                  height={30}
+                  variant="text"
+                  sx={{ fontSize: "1rem" }}
+                />
+              ) : (
+                <>
+                  <tr className="text-white">
+                    <th>No</th>
+                    <th>Recipient Name</th>
+                    <th>Require Blood Group</th>
+                    <th>Location</th>
+                    <th>Donation Date</th>
+                    <th>Donation Time</th>
+                    <th>Donation Status</th>
+                  </tr>
+                </>
+              )}
+            </>
+          </thead>
+          <tbody>
+            {isLoading ? (
+              <LoadingSkeleton />
+            ) : (
+              <>
+                {filteredRequests.map((request, index) => (
+                  <tr key={request._id}>
+                    <th>{index + 1}</th>
+                    <td>{request.recipientName}</td>
+                    <td>{request.requiredBloodGroup}</td>
+                    <td>
+                      {request.upazila}, {request.district}
+                    </td>
+                    <td>{request.donationDate}</td>
+                    <td>{request.donationTime}</td>
+                    <td>{request.donationStatus}</td>
+
+                    <Link to={`/dashboard/details/${request._id}`}>
+                      {/* You can add any content or styling for the link */}
+                    </Link>
+                  </tr>
+                ))}
+              </>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
+  </div>
   );
 };
 
---
+++ client/src/pages/All Request/LoadingSkeleton.jsx
@@ -0,0 +1,163 @@
+import { Skeleton, Stack } from "@mui/material";
+
+
+
+
+const LoadingSkeleton = () => {
+    return (
+        <Stack spacing={2}>
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                  <Skeleton
+                    width="100vw"
+                    height={30}
+                    variant="text"
+                    sx={{ fontSize: "1rem" }}
+                  />
+                </Stack>
+    );
+};
+
+export default LoadingSkeleton;
