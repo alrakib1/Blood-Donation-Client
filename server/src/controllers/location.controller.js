47dab87fb9c6262f127e8eace74591cb53c2fbec
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-11-21 11:23:51 +0600

    add location controller and error handling utilities; update location routes

---
+++ server/src/controllers/location.controller.js
@@ -0,0 +1,25 @@
+import { District } from "../models/district.model.js";
+import { Division } from "../models/division.model.js";
+import ApiError from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getDivisions = asyncHandler(async (req, res) => {
+  const divisions = await Division.find();
+
+  console.log(divisions, "divisions");
+
+  res.status(200).json(new ApiResponse(200, divisions, "success"));
+});
+
+const getDistricts = asyncHandler(async (req, res) => {
+  const district = await District.find();
+
+  if (!district) {
+    return res.status(404).json(new ApiError(404, "No district found"));
+  }
+
+  res.status(200).json(new ApiResponse(200, district, "success"));
+});
+
+export { getDistricts, getDivisions };
--- server/src/routes/location.route.js
+++ server/src/routes/location.route.js
@@ -1,12 +1,12 @@
-import {Router} from "express";
-
-
+import { Router } from "express";
+import { getDivisions } from "../controllers/location.controller.js";
 
 const router = Router();
 
-router.get('/', (req,res)=>{
-    res.send("Hello World");
-})
+router.get("/", (req, res) => {
+  res.send("Hello from location route");
+});
 
+router.get("/divisions", getDivisions);
 
 export default router;
