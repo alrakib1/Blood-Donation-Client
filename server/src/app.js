77a26dc20d80f2209eb7bdeba3aa8d7e13133abe
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-11-21 11:09:00 +0600

    add location routing and middleware for JSON parsing

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,12 @@
 import express from "express";
+import locationRouter from "./routes/location.route.js";
 
 const app = express();
+
+
+app.use(express.json());
+
+
+app.use('/api/v1', locationRouter);
 
 export default app;
