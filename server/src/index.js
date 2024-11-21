eba198add422a6150b2fd9db7b3001df9a9cca74
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-11-21 10:32:34 +0600

    refactor database connection and restructure app entry point

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import { app } from "./app.js";
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
@@ -9,7 +9,9 @@ dotenv.config({
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running on : http://localhost:${process.env.PORT}`);
+      console.log(
+        `Server is running on : http://localhost:${process.env.PORT}`
+      );
     });
   })
   .catch((error) => console.log("MongoDb connection failed", error));
